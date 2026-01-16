7543784f6627294885aec531dac17c541ef2cd4f
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-16 11:30:07 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
-import '../styles/globals.css';
-import AppLayout from '../components/AppLayout';
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
-  );
+  return <Component {...pageProps} />;
 }
