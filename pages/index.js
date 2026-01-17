65def90c4aaf9d3d091d79f8e487cd8078741219
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-17 02:22:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,55 +4,60 @@ export default function Home() {
   return (
     <div className="hr-homeWrap">
       <div className="hr-homeCardStack">
-        <div className="hr-homeCard hr-homeHero">
-          <div className="hr-homeHeroTitle">Hrvatski U21/NT Tracker</div>
-          <div className="hr-homeHeroSub">
-            Javni pregled strukture i “preview”. Igrači i skillovi su zaključani bez prijave.
-          </div>
+        <div className="hr-3dCard">
+          <div className="hr-3dCardInner">
+            <div className="hr-homeHeroTitle">Hrvatski U21/NT Tracker</div>
+            <div className="hr-homeHeroSub">
+              Javni pregled strukture i “preview”. Igrači i skillovi su zaključani bez prijave.
+            </div>
 
-          <div className="hr-homeHeroBtns">
-            <Link className="hr-homeBtn" href="/team/u21">Hrvatska U21</Link>
-            <Link className="hr-homeBtn" href="/team/nt">Hrvatska NT</Link>
+            <div className="hr-homeHeroBtns">
+              <Link className="hr-homeBtn" href="/team/u21">Hrvatska U21</Link>
+              <Link className="hr-homeBtn" href="/team/nt">Hrvatska NT</Link>
+            </div>
           </div>
         </div>
 
-        <div className="hr-homeCard hr-homeMyPlayers">
-          <div className="hr-homeRow">
-            <div>
-              <div className="hr-homeCardTitle">Moji igrači u Hrvatskom trackeru</div>
-              <div className="hr-homeCardText">
-                CHPP spajanje dolazi kasnije. Za sada pripremamo UI + DB za “moji igrači” u globalnom trackeru.
+        <div className="hr-3dCard" style={{ marginTop: 12 }}>
+          <div className="hr-3dCardInner">
+            <div className="hr-homeRow">
+              <div>
+                <div style={{ fontWeight: 900 }}>Moji igrači u Hrvatskom trackeru</div>
+                <div style={{ fontSize: 13, opacity: 0.85, marginTop: 4 }}>
+                  CHPP spajanje dolazi kasnije. Za sada pripremamo UI + DB za “moji igrači” u globalnom trackeru.
+                </div>
               </div>
-            </div>
-
-            <div className="hr-homeRight">
-              <span className="hr-homePill">Prijava (CHPP kasnije)</span>
+              <div className="hr-homePill">Prijava (CHPP kasnije)</div>
             </div>
           </div>
         </div>
 
         <div className="hr-homeGrid">
-          <div className="hr-homeMiniCard">
-            <div className="hr-homeMiniHead">
-              <div className="hr-homeMiniTitle">Hrvatska U21</div>
-              <span className="hr-homeTag">Preview</span>
+          <Link className="hr-3dCard hr-3dHover" href="/team/u21" style={{ textDecoration: "none" }}>
+            <div className="hr-3dCardInner">
+              <div className="hr-homeMiniHead">
+                <div className="hr-homeMiniTitle">Hrvatska U21</div>
+                <span className="hr-homeTag">Preview</span>
+              </div>
+              <div className="hr-homeMiniText">
+                Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.
+              </div>
+              <div className="hr-homeMiniLink">Otvori →</div>
             </div>
-            <div className="hr-homeMiniText">Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.</div>
-            <div className="hr-homeMiniLink">
-              <Link href="/team/u21">Otvori →</Link>
-            </div>
-          </div>
+          </Link>
 
-          <div className="hr-homeMiniCard">
-            <div className="hr-homeMiniHead">
-              <div className="hr-homeMiniTitle">Hrvatska NT</div>
-              <span className="hr-homeTag">Preview</span>
+          <Link className="hr-3dCard hr-3dHover" href="/team/nt" style={{ textDecoration: "none" }}>
+            <div className="hr-3dCardInner">
+              <div className="hr-homeMiniHead">
+                <div className="hr-homeMiniTitle">Hrvatska NT</div>
+                <span className="hr-homeTag">Preview</span>
+              </div>
+              <div className="hr-homeMiniText">
+                Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.
+              </div>
+              <div className="hr-homeMiniLink">Otvori →</div>
             </div>
-            <div className="hr-homeMiniText">Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.</div>
-            <div className="hr-homeMiniLink">
-              <Link href="/team/nt">Otvori →</Link>
-            </div>
-          </div>
+          </Link>
         </div>
 
         <div className="hr-homeLinks">
