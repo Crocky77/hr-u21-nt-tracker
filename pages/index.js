15caad64c475db4e5c97d54764b19657aec607ec
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-17 09:45:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,43 +6,24 @@ export default function HomePage() {
     <AppLayout isHome>
       <div className="hr-homeWrap">
         <div className="hr-homeCardStack">
-          {/* HERO (jedini naslov) */}
-          <div className="hr-3dCard">
+          {/* HERO - samo naslov i kratki opis (u boji) */}
+          <div className="hr-3dCard hr-homeHeroCard">
             <div className="hr-3dCardInner">
               <div className="hr-homeHeroTitle">Hrvatski U21/NT Tracker</div>
               <div className="hr-homeHeroSub">
                 Javni pregled strukture i “preview”. Igrači i skillovi su zaključani bez prijave.
               </div>
-
-              {/* Linkovi u headeru (pregledno, ne ruši dizajn) */}
-              <div className="hr-homeNav">
-                <Link className="hr-homeNavLink" href="/about">O nama</Link>
-                <Link className="hr-homeNavLink" href="/help">Pomoć</Link>
-                <Link className="hr-homeNavLink" href="/donate">Donacije</Link>
-                <Link className="hr-homeNavLink" href="/privacy">Privacy</Link>
-                <Link className="hr-homeNavLink" href="/terms">Terms</Link>
-              </div>
-
-              {/* Glavni gumbi */}
-              <div className="hr-homeHeroBtns">
-                <Link className="hr-homeBtn hr-homeBtnU21" href="/team/u21">
-                  Hrvatska U21
-                </Link>
-                <Link className="hr-homeBtn hr-homeBtnNT" href="/team/nt">
-                  Hrvatska NT
-                </Link>
-              </div>
             </div>
           </div>
 
           {/* MOJI IGRACI */}
-          <div className="hr-3dCard" style={{ marginTop: 12 }}>
+          <div className="hr-3dCard hr-homeMyCard" style={{ marginTop: 12 }}>
             <div className="hr-3dCardInner">
               <div className="hr-homeRow">
                 <div>
                   <div style={{ fontWeight: 900 }}>Moji igrači u Hrvatskom trackeru</div>
                   <div style={{ marginTop: 6, opacity: 0.8, fontSize: 13 }}>
-                    CHPP spajanje dolazi kasnije. Za sada pripremamo UI + DB za “moji igrači” u globalnom trackeru.
+                    CHPP spajanje dolazi kasnije. Za sada pripremamo UI + DB za “moji igrači”.
                   </div>
                 </div>
 
@@ -51,18 +32,16 @@ export default function HomePage() {
             </div>
           </div>
 
-          {/* 2 KLIKABILNE KARTICE (U21 / NT) */}
+          {/* U21 / NT klikabilni moduli - bez “preview/otvori” */}
           <div className="hr-homeGrid">
             <Link href="/team/u21" className="hr-homeMiniCard hr-homeMiniCardU21">
               <div className="hr-3dCardInner">
                 <div className="hr-homeMiniHead">
                   <div className="hr-homeMiniTitle">Hrvatska U21</div>
-                  <span className="hr-homeTag">Preview</span>
                 </div>
                 <div className="hr-homeMiniText">
                   Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.
                 </div>
-                <div className="hr-homeMiniLink">Otvori →</div>
               </div>
             </Link>
 
@@ -70,14 +49,25 @@ export default function HomePage() {
               <div className="hr-3dCardInner">
                 <div className="hr-homeMiniHead">
                   <div className="hr-homeMiniTitle">Hrvatska NT</div>
-                  <span className="hr-homeTag">Preview</span>
                 </div>
                 <div className="hr-homeMiniText">
                   Pregled modula (preview). Igrači i skillovi su zaključani bez prijave.
                 </div>
-                <div className="hr-homeMiniLink">Otvori →</div>
               </div>
             </Link>
+          </div>
+
+          {/* Sitni linkovi (bez rušenja dizajna) */}
+          <div className="hr-homeBottomLinks">
+            <Link href="/about">O alatu</Link>
+            <span className="hr-dot">•</span>
+            <Link href="/help">Pomoć</Link>
+            <span className="hr-dot">•</span>
+            <Link href="/donate">Donacije</Link>
+            <span className="hr-dot">•</span>
+            <Link href="/privacy">Privacy</Link>
+            <span className="hr-dot">•</span>
+            <Link href="/terms">Terms</Link>
           </div>
 
           <div className="hr-homeNote">
@@ -87,4 +77,4 @@ export default function HomePage() {
       </div>
     </AppLayout>
   );
-            }
+}
