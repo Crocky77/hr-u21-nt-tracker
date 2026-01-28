ee0afa2bf42e239f6d2721eefcfde906886f4632
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-28 11:06:33 +0100

    Update intro.js

--- pages/intro.js
+++ pages/intro.js
@@ -1,14 +1,16 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import Lottie from "lottie-react";
 
 export default function IntroPage() {
   const router = useRouter();
 
   useEffect(() => {
+    // load lottie web component
+    import("@lottiefiles/lottie-player");
+
     const timer = setTimeout(() => {
       router.replace("/");
-    }, 5200); // 5.2 s
+    }, 5200);
 
     return () => clearTimeout(timer);
   }, [router]);
@@ -19,17 +21,13 @@ export default function IntroPage() {
         width: "100vw",
         height: "100vh",
         backgroundColor: "#1E7F43",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         overflow: "hidden",
       }}
     >
-      <Lottie
-        animationData={null}
-        path="/intro.json"
-        loop={false}
+      <lottie-player
+        src="/intro.json"
         autoplay
+        loop={false}
         style={{
           width: "100%",
           height: "100%",
