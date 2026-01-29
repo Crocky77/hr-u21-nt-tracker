a9ab9e65b294b47856e2f2125517a069fab52367
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-29 23:29:29 +0100

    Update TopBar.js

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,38 +1,4 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-
 export default function TopBar() {
-  const router = useRouter();
-
-  const isActive = (href) => router.pathname === href;
-
-  return (
-    <header className="topbar">
-      <div className="topbar__left">
-        <Link href="/" className="topbar__brand" aria-label="Naslovnica">
-          <img
-            src="/logo.png"
-            alt="HR U21/NT Tracker"
-            className="topbar__logo"
-          />
-          <span className="topbar__title">HRVATSKI U21 / NT TRACKER</span>
-        </Link>
-      </div>
-
-      <nav className="topbar__nav" aria-label="Glavna navigacija">
-        <Link
-          href="/"
-          className={`topbar__link ${isActive("/") ? "is-active" : ""}`}
-        >
-          Naslovnica
-        </Link>
-        <Link href="/team/nt" className="topbar__link">
-          NT
-        </Link>
-        <Link href="/team/u21" className="topbar__link">
-          U21
-        </Link>
-      </nav>
-    </header>
-  );
+  // TopBar je isključen (koristimo zajednički Header iz Layout-a)
+  return null;
 }
