6c875acd852347653663e4024b72a9f0372f3d4b
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 20:37:44 +0100

    Create TileTransfer.js

---
+++ components/home/TileTransfer.js
@@ -0,0 +1,11 @@
+export default function TileTransfer() {
+  return (
+    <div className="home-tile transfer disabled">
+      <div className="home-transferIcon">⇅</div>
+      <div className="home-transferTitle">Transfer lista</div>
+      <div className="home-transferSub">
+        Privremeno nedostupno (CHPP)
+      </div>
+    </div>
+  );
+}
