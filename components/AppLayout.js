c2c25cede88ebf09d4b16c47ecad31e4ab0e60a8
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 01:35:19 +0100

    Update AppLayout.js

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function AppLayout({ children, user = null }) {
   return (
     <>
       <Header user={user} />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
