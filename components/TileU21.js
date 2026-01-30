14d1be7cc10eb43e434d4c791e6b9ffe2ffa3eae
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 20:51:24 +0100

    Create TileU21.js

---
+++ components/TileU21.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function TileU21() {
+  return (
+    <Link
+      href="/team/u21"
+      className="home-tile image u21"
+      aria-label="U21 Hrvatska"
+      title="U21 Hrvatska"
+    />
+  );
+}
