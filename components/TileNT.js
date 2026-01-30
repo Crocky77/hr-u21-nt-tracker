37b80add55ebc74ef5101b3be687105b0a0afade
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 20:50:50 +0100

    Create TileNT.js

---
+++ components/TileNT.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function TileNT() {
+  return (
+    <Link
+      href="/team/nt"
+      className="home-tile image nt"
+      aria-label="NT Hrvatska"
+      title="NT Hrvatska"
+    />
+  );
+}
