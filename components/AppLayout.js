3fbf7cb14686493a60e7280622a2bba07fe6c325
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 19:28:40 +0100

    Update AppLayout.js

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,9 +1,9 @@
-import TopBar from "./TopBar";
+import Header from "./Header";
 
 export default function AppLayout({ children, fullWidth = false }) {
   return (
     <div className="hr-appBg">
-      <TopBar />
+      <Header />
 
       <main className="hr-main" style={{ padding: fullWidth ? 0 : "20px 18px" }}>
         {fullWidth ? children : <div className="hr-container">{children}</div>}
