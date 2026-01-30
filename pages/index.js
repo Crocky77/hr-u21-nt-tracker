ab95db8bd48da919074b74ff6b2f0e49ee148e4a
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-30 20:59:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Link from "next/link";
 import Header from "../components/Header";
-import TileNT from "../components/TileNT";
-import TileU21 from "../components/TileU21";
-import TileTransfer from "../components/TileTransfer";
 
 export default function HomePage() {
   return (
@@ -10,34 +8,52 @@ export default function HomePage() {
 
       <main className="hr-main">
         <div className="hr-container">
-
+          {/* GLAVNI PANEL – kao referentna slika */}
           <div className="home-mainPanel">
-
-            <h1 className="home-panelTitle">
-              Moji igrači u Hrvatskom trackeru
-            </h1>
+            <h1 className="home-panelTitle">Moji igrači u Hrvatskom trackeru</h1>
 
             <p className="home-panelSubtitle">
               CHPP dozvola je kasnije. Za sada pripremamo UI + DB za “moji igrači”.
             </p>
 
+            {/* LOGIN – mora biti klikabilno, ali nenametljivo */}
             <div className="home-loginRow">
-              <button className="home-loginBtn ghost">
+              <Link className="home-loginBtn ghost" href="/login">
                 Admin / Tester login
-              </button>
-              <button className="home-loginBtn disabled">
+              </Link>
+
+              {/* CHPP kasnije – za sada disabled */}
+              <span className="home-loginBtn disabled" title="CHPP login će biti aktivan nakon licence">
                 CHPP login (uskoro)
-              </button>
+              </span>
             </div>
 
+            {/* 3 DOMINANTNA TILE-A – 1. red (NT / U21 / Transfer) */}
             <div className="home-tilesRow">
-              <TileNT />
-              <TileU21 />
-              <TileTransfer />
+              {/* NT – slika je widget, nema teksta preko */}
+              <Link
+                href="/team/nt"
+                className="home-tile image nt"
+                aria-label="NT Hrvatska"
+                title="NT Hrvatska"
+              />
+
+              {/* U21 */}
+              <Link
+                href="/team/u21"
+                className="home-tile image u21"
+                aria-label="U21 Hrvatska"
+                title="U21 Hrvatska"
+              />
+
+              {/* Transfer – privremeno disabled (CHPP) */}
+              <div className="home-tile transfer" aria-label="Transfer lista (CHPP)" title="Transfer lista">
+                <div className="home-transferIcon">⇅</div>
+                <div className="home-transferTitle">Transfer lista</div>
+                <div className="home-transferSub">Privremeno nedostupno (CHPP)</div>
+              </div>
             </div>
-
           </div>
-
         </div>
       </main>
     </div>
