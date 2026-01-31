0388a4fda7a655f91b4cf5b548306a1942bbb693
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-01-31 10:52:21 +0100

    Update TileFeatured.js

--- components/TileFeatured.js
+++ components/TileFeatured.js
@@ -1,17 +1,23 @@
 import Link from "next/link";
+import styles from "./TileFeatured.module.css";
 
 export default function TileFeatured({ href, image, label }) {
   return (
-    <Link href={href} className="featured-tile" aria-label={label} title={label}>
-      <div className="featured-frame">
-        <div className="featured-inner">
+    <Link
+      href={href}
+      className={styles.featuredTile}
+      aria-label={label}
+      title={label}
+    >
+      <div className={styles.frame}>
+        <div className={styles.inner}>
           <img
             src={image}
             alt={label}
-            className="featured-image"
+            className={styles.image}
             draggable="false"
           />
-          <div className="featured-mask" />
+          <div className={styles.mask} />
         </div>
       </div>
     </Link>
