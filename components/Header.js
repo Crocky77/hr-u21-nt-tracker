81aba4e25813ed4801a67cd9fc180d0d69b23a18
Crocky77 <67504566+Crocky77@users.noreply.github.com>
2026-02-08 11:33:12 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useUser } from "../lib/useUser";
+import AnnouncementTicker from "./AnnouncementTicker";
 
 export default function Header() {
   const router = useRouter();
@@ -45,6 +46,7 @@ export default function Header() {
           ) : null}
         </div>
       </div>
+      <AnnouncementTicker />
     </header>
   );
 }
